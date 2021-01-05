d1de3e1d69d56174dbcac9f501ce30c47fcb18e4
Lucas Viana <oviana@outlook.com>
2021-01-04 21:11:01 -0300

    update script fix

--- update-script/update.py
+++ update-script/update.py
@@ -3,4 +3,4 @@ import os
 
 with open('libraries.json', 'r') as f: libraries = json.load(f)
 
-for lib in libraries: os.system(f'cd ..; python3 plexmngcollections.py --l {lib["name"]} --t {lib["type"]} -g -s -y')
+for lib in libraries: os.system(f'cd ..; python3 plexmngcollections.py -l {lib["name"]} -t {lib["type"]} -g -s -y')
